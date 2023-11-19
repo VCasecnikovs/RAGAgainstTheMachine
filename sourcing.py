18f0c99b61daef3eb1189764ec71720226ef17d9
Emil Bogomolov <emilzq@bk.ru>
2023-11-18 18:07:18 -0800

    Revert "add filter_urls"

--- sourcing.py
+++ sourcing.py
@@ -1,8 +1,6 @@
 import requests
 import os
 from dotenv import load_dotenv
-from chatting import ChatMessage, chat_inference, get_openAI_client, Role
-import json
 
 load_dotenv()
 
@@ -103,7 +101,7 @@ def get_newsapi_news(query: str):
 SOURCES = {
     "you_news": get_you_news,
     "you_search":  get_you_search,
-    # "news_api": get_newsapi_news,
+    "news_api": get_newsapi_news,
 }
 
 
@@ -113,52 +111,6 @@ def get_data(query: str):
         results.append(get_func(query))
     return results
 
-def extract_urls(sources):
-    urls = []
-    for source in sources:
-        for article in source:
-            urls.append(article["url"])
-    return urls
-
-def filter_urls(urls):
-    client = get_openAI_client()
-
-        
-    with open('news_sources.json', 'r') as file:
-        data = json.load(file)
-
-    json_string = json.dumps(data[0], indent=4)
-
-    messages_list = [
-        ChatMessage(
-            role=Role.SYSTEM,
-            content=f"""User will send you list of URLs. Based on your knowledge and the list below, assign them to the listed categories:
-            
-            Categories: {json_string}
-            
-            Return JSON format:
-            "sources": {{
-                "left": [<items>],
-                "lean left": [<items>],
-                "center": [<items>],
-                "lean right": [<items>],
-                "right": [<items>]
-            }}
-            """
-        ),
-        ChatMessage(role=Role.SYSTEM, content=f"""URLs: {urls}"""),
-    ]
-
-    assistant_answer = chat_inference(client=client, messages=messages_list)
-    try:
-        parsed_json = json.loads(assistant_answer)
-        return parsed_json
-    except json.JSONDecodeError as e:
-        print("Error parsing JSON:", e)
-        return {"sources": {"any": urls}}
 
 if __name__ == '__main__':
-    sources = get_data("Xi Jinping in San Francisco")
-    urls = extract_urls(sources)
-    res = filter_urls(urls)
-    import pdb; pdb.set_trace()
+    print(get_data("Xi Jinping in San Francisco"))
